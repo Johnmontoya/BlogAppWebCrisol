05b314af666ad080b0afc2d30c1812b5bbeb194b
John Montoya <johnmontoya777@hotmail.com>
2025-11-30 12:02:12 -0500

    Cambio de folder de github y allow origin en app.js

--- server-crisol/src/app.js
+++ server-crisol/src/app.js
@@ -18,7 +18,7 @@ const app = express();
 const api = process.env.API_URL;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Reemplaza con el origen de tu frontend
+    origin: 'https://blog-app-web-crisol.vercel.app', // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Si necesitas manejar cookies o cabeceras de autorización
     optionsSuccessStatus: 204 // Código de estado para respuestas OPTIONS exitosas
