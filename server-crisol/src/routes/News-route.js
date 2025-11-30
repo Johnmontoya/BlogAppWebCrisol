48beae4a5a3ada98ac77d71b46edabe7eefd05ef
John Montoya <johnmontoya777@hotmail.com>
2025-11-29 23:19:20 -0500

    Swagger news documentacion solo GET

--- server-crisol/src/routes/News-route.js
+++ server-crisol/src/routes/News-route.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { addNews, deleteNews, newsAll, stateNews } from '../controllers/News-controller.js'; 
 import upload from '../middlewares/multer-middleware.js'
-import { LogUser } from '../middlewares/auth-middleware.js';
+import { LogAdmin } from '../middlewares/auth-middleware.js';
 
 const newsRouter = express.Router();
 
-newsRouter.post("/add", upload.single('image'), addNews);
+newsRouter.post("/add", upload.single('image'), LogAdmin, addNews);
 newsRouter.get('/all', newsAll);
-newsRouter.delete('/delete/:newsId', deleteNews);
-newsRouter.put('/statenews/:newsId', stateNews);
+newsRouter.delete('/delete/:newsId', LogAdmin, deleteNews);
+newsRouter.put('/statenews/:newsId', LogAdmin, stateNews);
 
 export default newsRouter;
