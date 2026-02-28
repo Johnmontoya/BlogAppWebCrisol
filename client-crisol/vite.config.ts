5d3cf6baa965011e5eb80d6c42821b98c368626c
John Montoya <johnmontoya777@hotmail.com>
2026-02-28 10:48:13 -0500

    permisos de origen

--- client-crisol/vite.config.ts
+++ client-crisol/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     allowedHosts: [
-      //"https://backendcrisolideas.onrender.com"
+      "https://backendcrisolideas.onrender.com"
     ]
   }
 })
